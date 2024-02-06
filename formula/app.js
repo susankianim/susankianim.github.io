2e8950b140ab72b98ebc3f42e40d199890185440
Susan Kiani <susankianim@gmail.com>
2024-02-06 11:33:45 +0330

    fixed bugg in formula

--- formula/app.js
+++ formula/app.js
@@ -309,12 +309,13 @@
   function update({ isWellFormed, cnfFormula }) {
     try {
       let isWellFormedContainer = document.getElementById("isWellFormed-container");
+      let cnfFormulaContainer = document.getElementById("cnfFormula-container");
       if (isWellFormed) {
         isWellFormedContainer.innerHTML = "This formula is well-formed, and here is its CNF:";
-        let cnfFormulaContainer = document.getElementById("cnfFormula-container");
         cnfFormulaContainer.innerHTML = cnfFormula;
       } else {
         isWellFormedContainer.innerHTML = "This formula is not well-formed.";
+        cnfFormulaContainer.innerHTML = "";
       }
     } catch (e) {
       console.log(e);
